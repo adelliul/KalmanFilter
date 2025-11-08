fd2392cf40fce4d499d9ad09bf9320b9db5e0b1b
adelliul <adelliaber@gmail.com>
2025-11-08 18:19:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 #include <iostream>
 #include <vector>
-#include "KalmanFilter.h"  \
+#include "KalmanFilter.h"  
 
 int main() {
-    // Example data: ]noisy price readings
+    // Example data: noisy price readings
     std::vector<double> prices = {5050.25, 5050.5, 5050.75, 5049.75, 5051.0, 5050.8, 5051.5};
 
     // Create a KalmanFilter object
@@ -14,7 +14,7 @@ int main() {
     // initial_estimate = first price
     KalmanFilter kf(0.01, 0.25, prices[0]);
 
-    std::cout << "NoisyPrice\tFilteredPrice\n";
+    std::cout << "NoisyPrice FilteredPrice";
 
     // Loop through each price and update our estimate
     for (double price : prices) {
